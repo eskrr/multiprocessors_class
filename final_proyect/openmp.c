633ac24ff797e2f641d7efe2f245dcc5f201fc07
eskrr <seesga97@gmail.com>
2021-05-31 01:01:16 -0500

    a

--- final_proyect/openmp.c
+++ final_proyect/openmp.c
@@ -3,7 +3,7 @@
 
 int main() {
 	printf("Max number of threads: %d", omp_get_max_threads());
-	printf("Current number of threads: %d", omp_get_num_threads());
+	printf("Current number of threads: %d", OMP_NUM_THREADS);
 
 	return 1;
 }
