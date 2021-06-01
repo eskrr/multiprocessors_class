f58fee9eba927ed99803c79617f937995a9fb002
eskrr <seesga97@gmail.com>
2021-06-01 16:58:47 -0500

    Adds cuda file

--- final_proyect/cuda.cu
+++ final_proyect/cuda.cu
@@ -9,26 +9,6 @@
 
 clock_t start, end;
 
-void printDeviceInfo() {
-	int nDevices;
-	cudaGetDeviceCount(&nDevices);
-	int i = 0;
-	for(i = 0; i < nDevices; i++) {
-		cudaDeviceProp prop;
-		cudaGetDeviceProperties(&prop, i);
-		printf("Device Number: %d\n", i);
-		printf("  Device name: %s\n", prop.name);
-		printf("  Maximum number of 32-bit registers: %d\n", prop.regsPerBlock);
-		printf("  Maximum number of threads per block: %d\n", prop.maxThreadsPerBlock);
-		printf("  Maximum block dimension: [%d,%d,%d]\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
-		printf("  Maximum grid size: [%d,%d,%d]\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
-		printf("  Compute Capability: %d.%d\n", prop.major, prop.minor);
-		printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
-		printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
-		printf("  Peak Memory Bandwidth (GB/s): %f\n\n", 2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
-	}
-}
-
 __global__ void calculateMatrixCuda(int *workPerThread, MATRIX* mA, MATRIX* mB, MATRIX* mC) {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;  // Calculate index for each thread
 	int startPos = idx * *workPerThread;
@@ -42,9 +22,6 @@ __global__ void calculateMatrixCuda(int *workPerThread, MATRIX* mA, MATRIX* mB, 
 }
 
 int main(int argc, char *argv[]) {
-	if (DEBUG)
-		printDeviceInfo();
-
 	if (!verifyArgs(argc))
 		return false;
 
