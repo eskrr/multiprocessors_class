b70c3575cbcff302fa4862768334eaaf6f154e81
eskrr <seesga97@gmail.com>
2021-06-02 22:33:00 -0500

    Force push

--- final_proyect/main.cu
+++ final_proyect/main.cu
@@ -9,6 +9,8 @@
 #define DEBUG false
 #define CUDA true
 #define NUM_TESTS 5
+#define MAX_THREADS 2014
+#define MAX_BLOCKS 1024
 
 clock_t start, end;
 
@@ -69,6 +71,64 @@ void runOmp(MATRIX* mA, MATRIX* mB, MATRIX* mC, double* times) {
 	}
 }
 
+void runCuda(MATRIX* mA, MATRIX* mB, MATRIX* mC, double* times) {
+	clock_t start, end;
+	int i = 0;
+
+	int *workPerThread;
+	cudaMallocManaged(&workPerThread, sizeof(int));
+
+	int totalBlocks = mC->rows < MAX_BLOCKS ?  mC->rows : MAX_BLOCKS;
+	int totalRows = mC->cols < MAX_THREADS ?  mC->cols : MAX_THREADS;
+
+	int totalWork = mC->rows * mC->cols;
+	*workPerThread = totalWork / (totalBlocks * totalRows);
+
+	double totalTime;
+	for (; i < NUM_TESTS; i++) {
+		start = clock();
+
+		calculateMatrixCuda <<<totalBlocks, totalRows>>> (workPerThread, mA, mB, mC);
+		cudaDeviceSynchronize();
+
+    	end = clock();
+
+    	totalTime = ((double) (end - start)) / CLOCKS_PER_SEC;
+    	*(times + i) = totalTime;
+    	memset(mC->vals, 0, (mC->rows * mC->cols)*sizeof(double));
+	}
+}
+
+__global__ void calculateMatrixCuda(int *workPerThread, MATRIX* mA, MATRIX* mB, MATRIX* mC) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;  // Calculate index for each thread
+	int pos = idx * *workPerThread;
+	int endPos = pos + *workPerThread;
+
+	int n = mA->cols;
+	for (; pos < mC->rows * mC->cols && pos < endPos; pos++) {
+		int row = pos / mC->rows;
+		int col = pos % mC->cols;
+
+		double sum = 0.0;
+		int i = 0;
+		for (; i < n; i++) {
+			// double valA, valB;
+
+			// valA = *matrixValue(mA, row, i);
+			// valB = *matrixValue(mB, i, col);
+
+			sum += (*(mA->vals + mA->cols * row + i) * *(mA->vals + mA->cols * i + col));
+		}
+
+		*(mC->vals + pos) = sum;
+	}
+
+	// printf("(ThreadId: %d, WorkPerThread: %d)\n", idx, *workPerThread);
+	// printf("(start: %d, end: %d)\n", startPos, endPos);
+	// printf("mA: rows: %d, cols: %d\n", mA->rows, mA->cols);
+	// printf("mB: rows: %d, cols: %d\n", mB->rows, mB->cols);
+	// printf("mC: rows: %d, cols: %d\n", mC->rows, mC->cols);
+}
 
 int main(int argc, char *argv[]) {
 	if (!verifyArgs(argc))
@@ -101,47 +161,8 @@ int main(int argc, char *argv[]) {
 	double *ompTimes = (double *)malloc(NUM_TESTS * sizeof(double));
 	runOmp(mA, mB, mC, ompTimes);
 
-	// MATRIX* mC;
-
-	// if  ((mC = initializeOutputMatrix(*mA, *mB, false)) == NULL) {
-	// 	printf("Error allocating output matrix C.\n");
-	// 	return -1;
-	// }
-   
- //   	start = clock();
-	// multiplyMatrix(
-	// 	/* startPos */ 	0,
-	// 	/* endPos */ 	mC->rows * mC->cols,
-	// 	/* matrix A */ 	*mA,
-	// 	/* matrix B */ 	*mB,
-	// 	/* matrix C */ 	mC);
- //    end = clock();
-
-	// if (DEBUG)
-	// 	printMatrix(*mC, 'C');
-
-
-	// MATRIX* mCT;
-
-	// if  ((mCT = initializeOutputMatrix(*mA, *mB, false)) == NULL) {
-	// 	printf("Error allocating output matrix C.\n");
-	// 	return -1;
-	// }
-   
- //   	start = clock();
-	// multiplyMatrixTransposed(
-	// 	/* startPos */ 	0,
-	// 	/* endPos */ 	mCT->rows * mCT->cols,
-	// 	/* matrix A */ 	*mA,
-	// 	/* matrix B */ 	*mBT,
-	// 	/* matrix C */ 	mC);
- //    end = clock();
-
-	// if (DEBUG)
-	// 	printMatrix(*mC, 'T');
-
- // 	double totalTime = (double)(end - start) / CLOCKS_PER_SEC;
- //    printf("Total time taken by CPU: %lf\n", end - start); 
+	double *cudaTimes = (double *)malloc(NUM_TESTS * sizeof(double));
+	runCuda(mA, mB, mC, cudaTimes);
 
 	freeMatrix(mA, CUDA);
 	freeMatrix(mB, CUDA);
@@ -151,7 +172,7 @@ int main(int argc, char *argv[]) {
 
 	int i;
 	for (i = 0; i < NUM_TESTS; i++)
-		printf("%20lf %20lf \n", *(serialTimes + i), *(ompTimes + i));
+		printf("%20lf %20lf \n", *(serialTimes + i), *(ompTimes + i), *(cudaTimes + i));
 
 	printf("\n");
 
